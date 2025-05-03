a57edf66592909e140b266a649f5128561a6392f
AliRaza <wolfhuntin49.com>
2025-05-03 10:58:36 +0500

    fix: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import AboutSection from "@/components/AboutSection/AboutSection";
-import Contact from "@/components/ContactSection/ContactSection";
+import Contact from "@/components/ContactSection/ContactSection"
 import ModernHeroSection from "@/components/HeroSection/HeroSection";
 import ProjectsSection from "@/components/ProjectSection/ProjectSection";
 import SkillsSection from "@/components/SkillSection/SkillSection";
